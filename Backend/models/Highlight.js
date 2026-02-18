a98b02ab85bb223cc5e5112b9da9d83a0b57addc
saiteja <saitejavinukond684@gmail.com>
2026-02-18 21:05:13 +0530

    implemented highlights

--- Backend/models/Highlight.js
+++ Backend/models/Highlight.js
@@ -1,1 +1,30 @@
 // SAI BABU HW
+const mongoose = require('mongoose');
+
+const highlightSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    highlightedText: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+
+    },
+    updatedAt: {
+        type: Date,
+        defult: Date.now
+    }
+});
+module.exports = mongoose.model('Highlight', highlightSchema);
+
